4873548877e9053f123600f3767472ac7c383bc9
AwalRamadan <awalramadan686@gmail.com>
2024-03-24 02:54:07 +0900

    Tugas3

---
+++ app/src/main/java/com/example/tmobilecomputing1/HalamanContent.kt
@@ -0,0 +1,11 @@
+package com.example.tmobilecomputing1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HalamanContent : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_halaman_content)
+    }
+}
---
+++ app/src/main/java/com/example/tmobilecomputing1/halutama.kt
@@ -0,0 +1,11 @@
+package com.example.tmobilecomputing1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class halutama : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_halutama)
+    }
+}
